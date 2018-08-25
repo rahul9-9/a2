286109903252fb863781e6a6964e5c90a3183dfb
rahul9-9 <rahulvashist99@gmail.com>
2018-08-25 20:21:03 +0530

    work in progress

---
+++ new3.java
@@ -0,0 +1,17 @@
+class l4code1
+{
+	public static void main(String[] args)
+	{
+		int test=5;
+		if(test<10)
+		{
+			System.out.print("rahul");
+			
+		}
+		else
+		{
+		System.out.print("292");	
+		}
+	}
+	
+}
